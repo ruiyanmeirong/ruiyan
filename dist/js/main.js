ca961bb2e3e2f1d359662d05c91fadbb8c32ae82
ruijun.he <ruijun.he@travelzen.com>
2015-03-18 15:12:28 +0800

    2015-03-18

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,12 @@
 $(function(){
-    $('#home-type li').on('click', function(){
-        $('#home-type li').removeClass('current-type');
+    $('#home-type li,.home-type li').on('click', function(){
+        $('#home-type li, .home-type li').removeClass('current-type');
         $(this).addClass('current-type');
+    });
+
+    $('.home-type2 li').on('click', function(){
+        $('.home-type2 li').removeClass('current-type2');
+        $(this).addClass('current-type2');
     });
 
     $('#home-tab li').on('click', function(){
